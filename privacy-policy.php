a39f969964f9fe2e461d99fde73fc2b6daa6cb2a
vcburnett <victor@konversion.ca>
2017-03-14 15:55:50 -0400

    2 more pages
    - terms and conditions
    - privacy policy

---
+++ privacy-policy.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1" name="viewport" />
+<title>Joe's Fusion</title>
+
+<?php include_once("assets/modules/head.php"); ?>
+
+</head>
+
+<body>
+
+    <?php include_once("assets/modules/header.php"); ?>
+
+    <!-- WRAPPER Content -->
+    <div class="wrapper">
+		<div class="wrap-long-text">
+			<h1>Privacy Policy</h1>
+			<p class="subtitle-meta">Updated on April 10, 2017</p>
+			<p>Bacon ipsum dolor amet pork belly salami short ribs ball tip ham hock filet mignon shank flank meatball pork sausage boudin kevin. Kielbasa bresaola ball tip, ribeye brisket beef ribs jerky short ribs meatball pork andouille doner pastrami. Picanha bresaola bacon ribeye ground round pig spare ribs cow tail short loin meatloaf pancetta. Spare ribs bresaola shank jowl turkey fatback beef landjaeger. Sirloin salami rump cow burgdoggen, leberkas shoulder brisket chuck jowl pastrami jerky pork. Venison t-bone rump ground round sausage pork boudin hamburger short loin drumstick fatback pig.</p>
+			<p>Ground round ham boudin ham hock pork chop ball tip chuck turkey kielbasa jowl bresaola pastrami sirloin tenderloin sausage. Filet mignon spare ribs beef ribs, prosciutto doner jerky chuck porchetta. Prosciutto picanha ground round chuck shankle burgdoggen tail. Pork chop fatback tail drumstick, alcatra hamburger sirloin sausage. T-bone ground round turkey flank bresaola cow swine.</p>
+			<p>Salami bresaola pork belly pastrami, t-bone beef ribs brisket picanha shank meatball chicken tongue. Tail porchetta t-bone hamburger doner. Sausage picanha ribeye bacon boudin tongue, pork leberkas hamburger short loin brisket. Turkey jowl tenderloin ground round shankle tri-tip short loin tail landjaeger beef salami spare ribs pastrami biltong prosciutto. Capicola bresaola short ribs, cupim flank landjaeger bacon picanha jerky alcatra pig jowl rump beef ribs. Ground round jowl beef salami leberkas landjaeger pork belly turkey tenderloin. Tri-tip pastrami burgdoggen leberkas cow porchetta boudin tongue landjaeger rump sirloin andouille.</p>
+			<p>Burgdoggen salami pastrami leberkas cupim. Shank turducken leberkas, strip steak short loin venison swine pork loin. Prosciutto andouille ground round spare ribs. Rump filet mignon short loin, ham hock cupim short ribs venison bacon picanha. Boudin bacon kevin short loin, alcatra flank chicken meatball beef pastrami biltong short ribs.</p>
+			<p>Kevin strip steak salami pig fatback tongue swine pork alcatra short ribs brisket sirloin biltong spare ribs. Spare ribs brisket fatback short ribs rump beef pork loin capicola flank. Tongue porchetta venison, tenderloin pork belly pig turducken swine meatball biltong pork loin fatback beef ribs kielbasa. Shank picanha hamburger, leberkas pork belly tongue tail bacon filet mignon landjaeger. Kielbasa ground round beef ribeye beef ribs, cow leberkas kevin brisket capicola cupim shoulder corned beef. Meatball porchetta shankle, pork chop ribeye corned beef filet mignon turducken kevin prosciutto.</p>
+		</div>
+    </div>
+    <!-- end/ WRAPPER Content -->
+
+	<?php include_once("assets/modules/footer.php"); ?>
+
+</body>
+</html>
