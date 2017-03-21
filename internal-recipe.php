2900627ee2dfd7d3d23a283052013d090c8ad334
vcburnett <victor@konversion.ca>
2017-03-21 17:14:13 -0400

    Update to internal page and CSS

--- internal-recipe.php
+++ internal-recipe.php
@@ -144,7 +144,21 @@
 		</section>
 
 		<section class="author-wrapper">
-
+			<div class="author-thumb">
+				<a href="javascript:void(0);"><img src="assets/images/_examples/profile-example.jpg" alt="A thumb for related articles"></a>
+			</div>
+			<div class="author-content">
+				<h2>Gordon Ramsay</h2>
+				<p>Filet mignon shankle pork biltong. Beef ribs kevin sausage, jerky short ribs picanha leberkas pancetta meatball chuck venison meatloaf.</p>
+				<div class="author-social">
+					<a href="javascript:void(0);"></a>
+					<a href="javascript:void(0);"></a>
+					<a href="javascript:void(0);"></a>
+					<a href="javascript:void(0);"></a>
+				</div>
+				<a href="search.php">View more</a>
+				<a href="bio.php">More about {{Name}}</a>
+			</div>
 		</section>
 
 	</div>
